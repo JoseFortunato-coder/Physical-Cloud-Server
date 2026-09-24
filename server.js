5db0ef81299acc9f793906fc524ea7e905bd9fe9
José Augusto <jose.11881444@aluno.mg.gov.br>
2026-09-24 13:02:48 -0300

    feat: cria servidor WebSocket básico

---
+++ server.js
@@ -0,0 +1,8 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 3000 });
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+  });
+});
+console.log('WebSocket server is running on ws://localhost:3000');
