2f6629eda62ef3a2c8af248158514f271f9e2a19
José Augusto <jose.11881444@aluno.mg.gov.br>
2026-09-25 12:29:17 -0300

    feat: Servidor conectado e mostrando console.log quando o cliente se conecta a ele

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3000 });
 wss.on('connection', (ws) => {
+  console.log('Client connected');
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
   });
