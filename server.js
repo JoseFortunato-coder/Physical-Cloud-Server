170be49b6a8a9d5b6c2f35708d1d4ccf2fe5a7ae
José Augusto <jose.11881444@aluno.mg.gov.br>
2026-09-25 12:56:19 -0300

    feat: gera um código randomico de 6 digitos

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3000 });
+const clientes = new Map(); 
+let de = 100000;
+    let ate = 999999;
+      let codigo = Math.floor(Math.random() * (ate - de + 1)) + de
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  console.log(`Código gerado: ${codigo}`);
   ws.on('message', (message) => {
+    JSON.parse(message);
     console.log(`Received message: ${message}`);
   });
 });
